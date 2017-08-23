1edb59d17ea54e07f257278641695330963023af
Szymon Ramczykowski <s.ramczykowski@kainos.com>
2017-08-23 15:46:57 +0200

    proper local url

--- app/services/configService.js
+++ app/services/configService.js
@@ -2,6 +2,6 @@
 
 angular.module('shop').factory('configService', [function() {
 	return {
-		servicesBaseUrl : 'http://localhost:8080/atcm-shop-0.0.1-SNAPSHOT'
+		servicesBaseUrl : 'http://localhost:9420'
 	}
 }]);
